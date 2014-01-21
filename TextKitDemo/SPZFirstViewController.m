02937d3ac85c77e25aa307a0faa316e201c1dba0
Brad Woodard <bcwoodard@gmail.com>
2014-01-21 15:48:05 -0600

    Added method applyStyleToSelection

--- TextKitDemo/SPZFirstViewController.m
+++ TextKitDemo/SPZFirstViewController.m
@@ -23,6 +23,7 @@
 
 
 - (void)textSizeChangedWithNotification:(NSNotification *)notification;
+- (void)applyStyleToSelection:(NSString *)style;
 
 @end
 
@@ -47,33 +48,39 @@
 }
 
 - (IBAction)applyHeadlineStyle:(id)sender {
-    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleHeadline]];
-    _styleApplied = UIFontTextStyleHeadline;
+//    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleHeadline]];
+//    _styleApplied = UIFontTextStyleHeadline;
+    [self applyStyleToSelection:UIFontTextStyleHeadline];
 }
 
 - (IBAction)applySubHeadStyle:(id)sender {
-    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline]];
-    _styleApplied = UIFontTextStyleSubheadline;
+//    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline]];
+//    _styleApplied = UIFontTextStyleSubheadline;
+    [self applyStyleToSelection:UIFontTextStyleSubheadline];
 }
 
 - (IBAction)applyBodyStyle:(id)sender {
-    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleBody]];
-    _styleApplied = UIFontTextStyleBody;
+//    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleBody]];
+//    _styleApplied = UIFontTextStyleBody;
+    [self applyStyleToSelection:UIFontTextStyleBody];
 }
 
 - (IBAction)applyFootnoteStyle:(id)sender {
-    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleFootnote]];
-    _styleApplied = UIFontTextStyleFootnote;
+//    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleFootnote]];
+//    _styleApplied = UIFontTextStyleFootnote;
+    [self applyStyleToSelection:UIFontTextStyleFootnote];
 }
 
 - (IBAction)applyCaption1Style:(id)sender {
-    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleCaption1]];
-    _styleApplied = UIFontTextStyleCaption1;
+//    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleCaption1]];
+//    _styleApplied = UIFontTextStyleCaption1;
+    [self applyStyleToSelection:UIFontTextStyleCaption1];
 }
 
 - (IBAction)applyCaption2Style:(id)sender {
-    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleCaption2]];
-    _styleApplied = UIFontTextStyleCaption2;
+//    [_textView setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleCaption2]];
+//    _styleApplied = UIFontTextStyleCaption2;
+    [self applyStyleToSelection:UIFontTextStyleCaption2];
 }
 
 - (IBAction)toggleImage:(id)sender {
@@ -84,4 +91,27 @@
 {
     [_textView setFont:[UIFont preferredFontForTextStyle:_styleApplied]];
 }
+
+- (void)applyStyleToSelection:(NSString *)style
+{
+    // Get the range selected in the textView
+    NSRange range = [_textView selectedRange];
+    
+    // Create a new font with the selected style
+    UIFont *styledFont = [UIFont preferredFontForTextStyle:style];
+    
+    // Begin editing the text storage property of the textView
+    [_textView.textStorage beginEditing];
+    
+    // Create a dictionary with the new font as the value
+    // and the NSFontAttributeName property as a key
+    NSDictionary *dict = @{NSFontAttributeName : styledFont};
+    
+    // Assign the new attributes to the text storage object
+    [_textView.textStorage setAttributes:dict range:range];
+    
+    // End editing the text storage property
+    [_textView.textStorage endEditing];
+}
+
 @end
