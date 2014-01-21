af973f1675528d6d816a327e9e101ae51cdbe909
Brad Woodard <bcwoodard@gmail.com>
2014-01-21 14:51:59 -0600

    Initial Commit

---
+++ TextKitDemo/SPZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPZAppDelegate.h
+//  TextKitDemo
+//
+//  Created by Brad Woodard on 1/21/14.
+//  Copyright (c) 2014 Spartz, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextKitDemo/SPZViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPZViewController.h
+//  TextKitDemo
+//
+//  Created by Brad Woodard on 1/21/14.
+//  Copyright (c) 2014 Spartz, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPZViewController : UIViewController
+
+@end
